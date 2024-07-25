9b35255b19d0cc6c8fcaeef6173bf885d8958c22
AbhiramiiS <abhiramis5002@gmail.com>
2024-07-25 22:13:24 +0530

    add photo

---
+++ src/components/search.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const search = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 ol-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 ol-xl-12 col-xxl-12">
+                    <label htmlFor="" className="form-label">Enter title to be searched</label>
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 ol-xl-12 col-xxl-12"><button className="btn btn-primary">Search</button>
+                </div>
+            </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default search
